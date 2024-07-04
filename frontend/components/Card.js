c0faaf6c236b24dc9186d8784431118d6de3c5ec
James Montgomery <erenroseyeager@gmail.com>
2024-07-04 13:53:10 -0400

    Stylized the webpage using mainly REACT's stylized components and a minor bit of CSS

--- frontend/components/Card.js
+++ frontend/components/Card.js
@@ -1,15 +1,39 @@
 import React from "react"
 import Figure from "./Figure"
+import styled from "styled-components"
+
+const StyledCard = styled.div`
+  max-width: 1000px;
+  border: 10px solid gold;
+  background-color: navy;
+  text-indent: 30px;
+  padding-left: 30px;
+  padding-right: 30px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+
+  h1 {
+    font-family: 'Times New Roman', Times, serif;
+  }
+  
+  h2 {
+    border-bottom: 3px solid gold;
+    margin-bottom: 10px;
+    font-family: 'Times New Roman', Times, serif;
+    font-style: italic;
+    font-size: 30px;
+  }
+`
 
 export default function Card({title, text, imageURL, date}) {
     return (
-      <div className='card'>
+      <StyledCard className='card'>
         <h2>{title}</h2>
         <p>{text}</p>
         <Figure
           imageURL={imageURL}
           captionDate={date}
         />
-      </div>
+      </StyledCard>
     )
   }
--- frontend/components/Figure.js
+++ frontend/components/Figure.js
@@ -1,10 +1,27 @@
 import React from "react"
+import styled from "styled-components"
+
+const StyledFigure = styled.figure`
+
+  figcaption {
+  color:#FAF9F6;
+  font-style: italic;
+  text-align: center;
+}
+
+  img {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    padding-top: 30px;
+  }
+`
 
 export default function Figure({imageURL, captionDate}) {
     return (
-      <figure>
+      <StyledFigure>
         <img src={imageURL}/>
         <figcaption>Photograph taken on {captionDate}</figcaption>
-      </figure>
+      </StyledFigure>
     )
   }
